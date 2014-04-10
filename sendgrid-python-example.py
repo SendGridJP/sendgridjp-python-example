72b7ed9db430204c2dbf90207fef0c350b86cbd7
root <root@6d22f39da05e.(none)>
2014-04-10 03:33:37 +0000

    Edit 2

--- sendgrid-python-example.py
+++ sendgrid-python-example.py
@@ -1,24 +1,29 @@
 import sendgrid
-#import ConfigParser
+import configparser
 
-#config = ConfigParser.ConfigParser()
-#config.read('.env')
-#
-#sendgrid_username = config.get('SENDGRID', 'SENDGRID_USERNAME')
-#sendgrid_password = config.get('SENDGRID', 'SENDGRID_PASSWORD')
-#tos = config.get('SENDGRID', 'TOS').split(',')
-#from = config.get('SENDGRID', 'FROM')
+config = configparser.ConfigParser()
+config.read('.env')
 
-#sg = sendgrid.SendGridClient(sendgrid_username, sendgrid_password)
-#message = sendgrid.Mail()
-#message.add_to('John Doe <john@email.com>')
-#message.set_subject('Example')
-#message.set_html('Body')
-#message.set_text('Body')
-#message.set_from('Doe John <doe@email.com>')
-#status, msg = sg.send(message)
+sendgrid_username = config.get('SENDGRID', 'SENDGRID_USERNAME')
+sendgrid_password = config.get('SENDGRID', 'SENDGRID_PASSWORD')
+tos = config.get('SENDGRID', 'TOS').split(',')
+from_address = config.get('SENDGRID', 'FROM')
 
-#print sendgrid_username
-#print sendgrid_password
-#print tos
-#print from
+sg = sendgrid.SendGridClient(sendgrid_username, sendgrid_password)
+message = sendgrid.Mail()
+message.add_to(tos)
+message.set_from('送信者名 <' + from_address + '>')
+message.set_subject('[sendgrid-python-example] フクロウのお名前はfullnameさん')
+message.set_text('familyname さんは何をしていますか？\r\n 彼はplaceにいます。')
+message.set_html('<strong> familyname さんは何をしていますか？</strong><br />彼はplaceにいます。')
+message.set_substitutions({'fullname': ['田中 太郎', '佐藤 次郎', '鈴木 三郎']})
+#message.set_substitutions({"familyname": ["田中", "佐藤", "鈴木"]})
+#message.set_substitutions({"place": ["office", "home", "office"]})
+message.add_section('office', '中野')
+message.add_section('home', '目黒')
+message.add_category('カテゴリ1')
+
+status, msg = sg.send(message)
+
+print(status)
+print(msg)
