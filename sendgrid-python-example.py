c97684fa0933abfb082e1f37837c7602b2c9d181
root <root@d2d30e732918.(none)>
2014-04-10 03:00:53 +0000

    edit 1

--- sendgrid-python-example.py
+++ sendgrid-python-example.py
@@ -1,12 +1,24 @@
 import sendgrid
+#import ConfigParser
 
-sg = sendgrid.SendGridClient('YOUR_SENDGRID_USERNAME', 'YOUR_SENDGRID_PASSWORD')
+#config = ConfigParser.ConfigParser()
+#config.read('.env')
+#
+#sendgrid_username = config.get('SENDGRID', 'SENDGRID_USERNAME')
+#sendgrid_password = config.get('SENDGRID', 'SENDGRID_PASSWORD')
+#tos = config.get('SENDGRID', 'TOS').split(',')
+#from = config.get('SENDGRID', 'FROM')
 
-message = sendgrid.Mail()
-message.add_to('John Doe <john@email.com>')
-message.set_subject('Example')
-message.set_html('Body')
-message.set_text('Body')
-message.set_from('Doe John <doe@email.com>')
-status, msg = sg.send(message)
+#sg = sendgrid.SendGridClient(sendgrid_username, sendgrid_password)
+#message = sendgrid.Mail()
+#message.add_to('John Doe <john@email.com>')
+#message.set_subject('Example')
+#message.set_html('Body')
+#message.set_text('Body')
+#message.set_from('Doe John <doe@email.com>')
+#status, msg = sg.send(message)
 
+#print sendgrid_username
+#print sendgrid_password
+#print tos
+#print from
